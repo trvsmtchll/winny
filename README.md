952138ba0d0f1fa8987ad3de25f63085fce8154a
trvsmtchll <trvsmtchll@yahoo.com>
2020-08-11 13:35:47 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # winny
+
 Installs packages for windows users in ubuntu sub-machine
+
+- Open ubuntu shell on windows machine
+- Clone this repository with git ```https://github.com/trvsmtchll/winny.git```
+- ```chmod +x windows-ubuntu-setup.sh```
+
+
---
+++ windows-ubuntu-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#############################################################################################################################
+# Author - Travis Mitchell Aug 11th 2020
+# Installs packages for Ubuntu machine (for Windows users)
+#############################################################################################################################
+
+# Install azure cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Install jq
+sudo apt-get install -y jq
+
+# Install Terraform
+wget https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_linux_amd64.zip
+sudo unzip ./terraform_0.12.29_linux_amd64.zip -d /usr/local/bin/
+
+echo "Terraform installed"
+terraform -v
+
+# Prompt user to login
+echo "Type az login to authenticate with Azure console"
